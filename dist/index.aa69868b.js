0ec849e81a9a0b2343fd58885f893033161c8dc5
Ninjabel <85708915+Ninjabel@users.noreply.github.com>
2023-02-24 16:17:21 +0100

    Sass Project

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,42 @@
+function changeBackgroundColor() {
+    const buttons = document.querySelectorAll(".nav-button");
+    const body = document.body;
+    const colors = {
+        0: "#210121",
+        1: "#01210f",
+        2: "#011721",
+        3: "#210101"
+    };
+    buttons.forEach((button, index)=>{
+        button.addEventListener("click", ()=>{
+            body.style.background = `${colors[index]}`;
+        });
+    });
+}
+function restoreDefaultColor() {
+    const logo = document.querySelector(".logo");
+    const body = document.body;
+    const defaultColor = "#080b1c";
+    logo.addEventListener("click", ()=>{
+        body.style.background = defaultColor;
+    });
+}
+function visitSites() {
+    const logos = document.querySelectorAll(".sidebar-item");
+    const websiteURLs = {
+        0: "https://en.wikipedia.org/wiki/HTML",
+        1: "https://pl.wikipedia.org/wiki/CSS",
+        2: "https://en.wikipedia.org/wiki/JavaScript",
+        3: "https://en.wikipedia.org/wiki/Sass_(stylesheet_language)"
+    };
+    logos.forEach((logo, index)=>{
+        logo.addEventListener("click", ()=>{
+            window.open(websiteURLs[index]);
+        });
+    });
+}
+changeBackgroundColor();
+restoreDefaultColor();
+visitSites();
+
+//# sourceMappingURL=index.aa69868b.js.map
